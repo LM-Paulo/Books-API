0fe64fe37aa726a0c2ef79f91a03e4b8a0f81f43
LM-Paulo <plucasmoraisn@gmail.com>
2023-08-24 23:26:30 -0300

    Create project

---
+++ src/main/java/com/pnogueira/liberayapi/LiberayApiApplication.java
@@ -0,0 +1,13 @@
+package com.pnogueira.liberayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiberayApiApplication.class, args);
+    }
+
+}
