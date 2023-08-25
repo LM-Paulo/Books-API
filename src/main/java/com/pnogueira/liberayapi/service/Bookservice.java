f8d23aebf522fb6e3a5a73ae0301311cfe82112b
LM-Paulo <plucasmoraisn@gmail.com>
2023-08-25 14:47:53 -0300

    feature - create Bookservice

---
+++ src/main/java/com/pnogueira/liberayapi/service/Bookservice.java
@@ -0,0 +1,11 @@
+package com.pnogueira.liberayapi.service;
+
+import com.pnogueira.liberayapi.model.entity.BookEntity;
+
+public class Bookservice {
+
+
+    public BookEntity save(BookEntity any) {
+        return null;
+    }
+}
