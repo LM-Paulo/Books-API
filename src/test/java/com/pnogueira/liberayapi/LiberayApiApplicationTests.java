3174b083c79cf5e491b53729c40e74ce83b7218c
LM-Paulo <plucasmoraisn@gmail.com>
2023-08-24 23:26:58 -0300

    criação do primeiro teste

---
+++ src/test/java/com/pnogueira/liberayapi/LiberayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnogueira.liberayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/pnogueira/liberayapi/api/resource/BookControllerTest.java
@@ -0,0 +1,58 @@
+package com.pnogueira.liberayapi.api.resource;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    static String BOOK_API = "/api/books"; // define a rota da api
+
+    @Autowired
+    MockMvc mvc; // injeção para simular as requisições
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso.")
+    public void createBookTest() throws Exception {
+
+        String json = new ObjectMapper().writeValueAsString(null); //recebe um objeto e o transforma em json
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+        mvc
+                .perform(request)
+                .andExpect(status().isCreated() )
+                .andExpect(jsonPath("id").isNotEmpty() )
+                .andExpect(jsonPath("title").value("Meu livro") )
+                .andExpect(jsonPath("author").value("Autor") )
+                .andExpect(jsonPath("isbn").value("1213212") )
+        ;
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro de validação quando não houver dados suficientes.")
+    public void createInvalidBookTest(){
+
+    }
+}
