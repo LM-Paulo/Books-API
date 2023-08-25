22dd125bb654e0c5d8720e8b68d22dd5217b458b
LM-Paulo <plucasmoraisn@gmail.com>
2023-08-25 15:50:57 -0300

    feature - create modelMapper

--- src/main/java/com/pnogueira/liberayapi/LiberayApiApplication.java
+++ src/main/java/com/pnogueira/liberayapi/LiberayApiApplication.java
@@ -1,10 +1,16 @@
 package com.pnogueira.liberayapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiberayApiApplication {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LiberayApiApplication.class, args);
--- src/main/java/com/pnogueira/liberayapi/api/resource/BookController.java
+++ src/main/java/com/pnogueira/liberayapi/api/resource/BookController.java
@@ -1,6 +1,9 @@
 package com.pnogueira.liberayapi.api.resource;
 
 import com.pnogueira.liberayapi.api.dto.BookDTO;
+import com.pnogueira.liberayapi.model.entity.BookEntity;
+import com.pnogueira.liberayapi.service.Bookservice;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,10 +11,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/books")
 public class BookController {
 
+
+
+    private Bookservice service;
+    private ModelMapper modelMapper;
+
+    public BookController(Bookservice service, ModelMapper modelMapper) {
+        this.service = service;
+        this.modelMapper = modelMapper;
+
+    }
+
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto){
-        return dto;
+
+       BookEntity entity = modelMapper.map(dto,BookEntity.class);
+       entity = service.save(entity);
+       return modelMapper.map(entity, BookDTO.class);
 
     }
 }
