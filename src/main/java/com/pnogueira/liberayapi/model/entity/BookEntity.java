06f76a9e9af48c27d7a875d48e9c6afc46620c74
LM-Paulo <plucasmoraisn@gmail.com>
2023-08-25 14:47:42 -0300

    feature - create BookEntity

---
+++ src/main/java/com/pnogueira/liberayapi/model/entity/BookEntity.java
@@ -0,0 +1,13 @@
+package com.pnogueira.liberayapi.model.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookEntity {
+}
